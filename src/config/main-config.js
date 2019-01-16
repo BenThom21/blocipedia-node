3b1e5027a4efcc0f09c028451275b0dbca15429d
Ben <benthompson4496@gmail.com>
2019-01-16 17:49:17 -0600

    initial page rendering and partials

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const logger = require('morgan');
-// const path = require("path");
+const path = require("path");
+const viewsFolder = path.join(__dirname, "..", "views");
 // const bodyParser = require("body-parser");
 // const expressValidator = require("express-validator");
 
 module.exports = {
     init(app, express) {
-
-        app.use(logger('dev'));
-        
+        app.set("views", viewsFolder);
+        app.set("view engine", "ejs");
+        app.use(express.static(path.join(__dirname, "..", "assets")));
     }
 };
--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -3,6 +3,6 @@
 
 module.exports = {
     index(req, res, next){ 
-        res.send("Welcome to DevWiki");
+        res.render("static/index", {title: "Welcome to DevWiki"});
     }
   }
